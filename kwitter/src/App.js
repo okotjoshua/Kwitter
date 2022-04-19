6dce2a1bf5b3ae9c71df2d855f973db689ab8578
okotjoshua <okotjoshua43@gmail.com>
2022-04-19 16:45:14 +0300

    Init

---
+++ kwitter/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+
+    // BEM
+    <div className="app">
+      <h1>My name is Joshua.....building Kwitter</h1>
+
+      {/* Sidebar */}
+      
+      {/* Feed */}
+
+      {/* Widgets */}
+
+    </div>
+  );
+}
+
+export default App;
