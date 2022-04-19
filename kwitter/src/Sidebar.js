796b38fcc603eb6f8cdc6f1ae2d2a45e259ec6ff
okotjoshua <okotjoshua43@gmail.com>
2022-04-19 19:35:28 +0300

    Init

---
+++ kwitter/src/Sidebar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './Sidebar.css'
+
+function Sidebar() {
+    return(
+        <div className="sidebar">
+             {/* Kwitter Icon */}
+             {/* SidebarOption */}
+             {/* SidebarOption */}
+             {/* SidebarOption */}
+             {/* SidebarOption */}
+             {/* SidebarOption */}
+             {/* SidebarOption */}
+             {/* Button -> Kweet */}
+        </div>
+    ); 
+}
+export default Sidebar;
