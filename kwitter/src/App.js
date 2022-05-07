af0e2d9bac56e2afde30ccc48e8b2858b1d00bf9
okotjoshua <okotjoshua43@gmail.com>
2022-05-07 23:30:13 +0300

    More

--- kwitter/src/App.js
+++ kwitter/src/App.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
@@ -7,6 +9,7 @@ function App() {
       <h1>Hey World......lets build Kwitter for KaKebe!!!</h1>
 
       {/* Sidebar */}
+      <Sidebar />
 
       {/* Feed */}
 
--- kwitter/src/Sidebar.js
+++ kwitter/src/Sidebar.js
@@ -1,21 +1,34 @@
 import React from "react";
 import './Sidebar.css'
+import BikeScooterIcon from '@mui/icons-material/BikeScooter';
+import SidebarOption from "./SidebarOption";
+import HomeIcon from '@mui/icons-material/Home';
+import TagIcon from '@mui/icons-material/Tag';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import MailOutlineOutlinedIcon from '@mui/icons-material/MailOutlineOutlined';
+import BookmarkBorderOutlinedIcon from '@mui/icons-material/BookmarkBorderOutlined';
+import ListAltOutlinedIcon from '@mui/icons-material/ListAltOutlined';
+import PermIdentityIcon from '@mui/icons-material/PermIdentity';
+import MoreHorizRoundedIcon from '@mui/icons-material/MoreHorizRounded';
+import { Button } from "@mui/material";
 
 function Sidebar(){
     return( 
         <div className="sidebar">
             {/* Kwitter icon */}
+            <BikeScooterIcon />
 
-            {/* SidebarOption */}
-            {/* SidebarOption */}
-            {/* SidebarOption */}
-            {/* SidebarOption */}
-            {/* SidebarOption */}
-            {/* SidebarOption */}
-            {/* SidebarOption */}
-            {/* SidebarOption */}
- 
+            <SidebarOption active Icon={HomeIcon} text="Home"/>
+            <SidebarOption Icon={TagIcon} text="Explore"/>
+            <SidebarOption Icon={NotificationsNoneOutlinedIcon} text="Notifications"/>
+            <SidebarOption Icon={MailOutlineOutlinedIcon} text="Messages"/>
+            <SidebarOption Icon={BookmarkBorderOutlinedIcon} text="Bookmarks"/>
+            <SidebarOption Icon={ListAltOutlinedIcon} text="Lists"/>
+            <SidebarOption Icon={PermIdentityIcon} text="Profile"/>
+            <SidebarOption Icon={MoreHorizRoundedIcon} text="More"/>
+
             {/* Button-> Kweet */}
+            <Button variant="outlined" className="sidebar_kweet" fullWidth>Kweet</Button>
             
 
         </div>
