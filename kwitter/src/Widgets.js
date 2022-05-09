2fe9f9f5a79d5549a9422321edabb3008a2c4f72
okotjoshua <okotjoshua43@gmail.com>
2022-05-09 14:10:28 +0300

    Continue

--- kwitter/src/Widgets.js
+++ kwitter/src/Widgets.js
@@ -1,14 +1,42 @@
-import React from 'react'
+import React from 'react';
 import "./Widgets.css";
+import {
+    TwitterTimelineEmbed,
+    TwitterShareButton,
+    TwitterTweetEmbed,
+
+} from "react-twitter-embed";
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 
 function Widgets() {
   return (
     <div className="widgets">
+        <div className="widgets_input">
+        <SearchOutlinedIcon className="widgets_searchIcon"/>
+        <input placeholder="Search Kwitter" type="text"/>
+        </div>
 
-        <h2>Right sidebar</h2>
+        <div className="widgets_widgetContainer">
+        <h2>What's trending?</h2>
 
+        <TwitterTweetEmbed tweetId={"1505197237755854859"}/>
+
+        <TwitterTweetEmbed tweetId={"1523282990419243008"}/>
+
+        <TwitterTweetEmbed 
+        sourceType="profile"
+        screenName="KelmoiRytah"
+        options={{ height:400 }}
+        />
+        
+        <TwitterShareButton
+            url={"https://facebook.com/cleverprogrammer"}
+            options={{ text: "#Kwitter is awesome", via: "okotjoshua"}}
+        />
+
+        </div>
     </div>
-  )
+  );
 }
 
 export default Widgets;
