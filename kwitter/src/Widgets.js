1edbcd132c1c48b96b8ae6dedc6397c9677d4420
okotjoshua <okotjoshua43@gmail.com>
2022-05-10 19:06:05 +0300

    continue

--- kwitter/src/Widgets.js
+++ kwitter/src/Widgets.js
@@ -18,6 +18,8 @@ function Widgets() {
       <div className="widgets__widgetContainer">
         <h2>What's trending?</h2>
 
+        <TwitterTweetEmbed tweetId={"1514299399861678080"} />
+
         <TwitterTweetEmbed tweetId={"858551177860055040"} />
 
         <TwitterTimelineEmbed
